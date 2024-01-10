56e330fd0302139a4ff6f08a4a9f6e63b68098c6
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-01-10 01:21:13 +0100

    update

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let quote = document.getElementById("quote");
 let author = document.getElementById("author");
 let btn = document.getElementById("btn");
 
-const url = "https://api.quotable.io/random";
+const ApiUrl = "https://api.quotable.io/random";
 
 async function Api() {
-    let response = await fetch(url);
+    let response = await fetch(ApiUrl);
     let result = await response.json();
     quote.innerHTML = result.content
     author.innerHTML = result.author
